6c9b4906d31d5a484c84517a4c79218d076b086a
moirahartigan <moirahartigan@gmail.com>
2021-06-14 20:01:55 +0000

    Add function names need to run game in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 // Memory card game logic
+function flipcard() {
+ 
+}
+
+
+
+function matched() {
+
+}
+
+
+
+function unmatched() {
+
+}
+
+function time() {
+
+
+}
+
+function shuffle() {
+
+}
+
+
+function reset() {
+    
+}
+
