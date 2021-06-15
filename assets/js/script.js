42a819092f4c525e8cd62c110c3afa7e2783b386
moirahartigan <moirahartigan@gmail.com>
2021-06-15 13:11:36 +0000

    Add data attribute to memory card class in html for flip card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,59 @@
-// Memory card game logic
+// Wait for the Dom to finish loading before running the game
+// get button elements and add event listeners to them
+
 const cards = document.querySelectorAll('.memory-card');
-let firstClick = false
-let cardPair = []
 
-cards.forEach((card) => {
-     card.state = 'unclicked'
-});
-
-// card flip
+let hasFlippedCard = false; //used to check if card has already been clicked 
+let firstCard, secondCard; //Used to check for cards match
+/*
+onclick function for cards, toggles flip class for css effects
+initial code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
+*/
 function flipCard() {
-    this.classList.toggle('flip');
+   
+    this.classList.toggle('flip'); //if valid, flips card using css class
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this; //stores this as first card
+    } else {
+        //second card
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log({firstCard, secondCard});
+
+        // do the cards match
+    }
+   
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+// run the game
+function runGame() {
+    
+}
+
+
 // card matching logic
-for(let i = 0; i < cards.length; i++){
-    cards[i].addEventListener('click',()=>{
-        if (!firstClick){time()}
-        firstClick=true;
 
-        if(cards[i].state == 'unclicked'){
-            cards[i].style.transform = 'rotateY(180deg)';
-            cards[i].state = 'clicked'
-            cardPair.push(cards[i]);
-            check()
-        }else if (cards[i].state == 'clicked'){
-            cards[i].style.transform = 'rotateY(0deg)'
-            cards[i].state = 'unclicked'
-            cardPair=[]
-        }
-    })
+function check() {
+   
 }
 
-function check(){
-    if(cardPair.length==2){
-        if(cardPair[0].querySelector('img').src==cardPair[1].querySelector('img').src){
-            matched()
-        }
-        else{
-            unmatched(cardPair[0],cardPair[1])
-        }
-    }  
-}
-
-function matched() {
+function correctMatch() {
   
 }
 
 
 
-function unmatched() {
+function incorrectMatch() {
 
 }
 
-function time() {
+function timer() {
     let secs = 0
     let mins = 0
     let SS
@@ -73,19 +72,7 @@ function time() {
 // shuffle the cards and loop through to make the cards appear in different places on each play
 // cards can be shuffled on load and restart
 function shuffle() {
-    let images = document.querySelectorAll('img')
-    let srcs = ['monster-1.jpg','monster-2.jpg','monster-3.jpg','monster-4.jpg','monster-5.jpg','monster-6.jpg','doctor.jpgmonster-7.jpg','monster-8.jpg']
-    
-    for(let i=srcs.length-1; i>0; i--){
-        let j = Math.floor(Math.random() * i)
-        let temp = srcs[i]
-        srcs[i] = srcs[j]
-        srcs[j] = temp
-      }
-      
-      for(let i=0; i<images.length; i++){
-          images[i].src = srcs[i]
-      }
+  
 }
 
 
