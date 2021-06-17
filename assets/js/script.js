dfdb8e8036b94d1e40b0c0192e447060938c69e9
moirahartigan <moirahartigan@gmail.com>
2021-06-17 17:37:20 +0000

    Add shuffle card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // define variables
 const cards = document.querySelectorAll('.memory-card');
-const winMsg = document.getElementById('win-msg');
-let hasFlippedCard = false; //used to check if card has already been clicked 
+
+
+let flippedCard = false; //used to check if card has already been clicked 
 let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
 let firstCard, secondCard; //Used to check for cards match
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
+shuffle();
 
 /*
 onclick function for cards, add flip class for css effects
@@ -14,35 +15,33 @@ code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and 
 */
 function flipCard() {
 if(lockBoard) return; // return is lockBoard is true so the rest of the function wont be executed
+if (this === firstCard) return;
 this.classList.add('flip'); //if valid, flips card using css class
 
-if (!hasFlippedCard){
-    // first click
-    hasFlippedCard = true;
+if (!flippedCard){
+    // The first card clicked
+    flippedCard = true;
     firstCard = this; //stores this as first card
    } else {
-       //second card
-       hasFlippedCard = false;
+       //The second card clicked
+       flippedCard = false;
        secondCard = this; 
        
-       checkforMatch();
-   }
-}
-
-function checkforMatch(){
-  //  do they match? - if not thay will be flipped back
-  if (firstCard.dataset.image ===
-    secondCard.dataset.image) {
+    //  Check if cards match? - if not thay will be flipped back
+  if (firstCard.dataset.image === secondCard.dataset.image) {
     disableCards(); 
     } else {
     unflipCards();  
-    } 
+    }     
+   }
 }
 
 function disableCards(){
 // if its a match the eventlistener is disabled
 firstCard.removeEventListener('click', flipCard);  
 secondCard.removeEventListener('click', flipCard); 
+
+resetBoard();
 }
 
 function unflipCards(){
@@ -52,21 +51,25 @@ function unflipCards(){
     firstCard.classList.remove('flip');
   secondCard.classList.remove('flip');
   
-  lockBoard = false;
-  }, 900);      
+  resetBoard();
+  }, 900);  
+  
 }
 
+function resetBoard(){
+  [flippedCard, lockBoard] = [false, false];
+  [firstCard,secondCard] = [null, null];
+}
 
+// Card shuffle
+function shuffle() {
+  cards.forEach(cards => {
+      let randomPosition = Math.floor(Math.random() * 16);
+      cards.style.order = randomPosition;
+  })
 
-          
- // let shuffleCard = [monster-1, monster-1, monster-2, monster-2, monster-3, monster-3, monster-4, monster-4, 
-   // monster-5, monster-5, monster-6, monster-6, monster-7, monster-7, monster-8, monster-8];
-
-    //function shuffleImages() {
-    //for (let i = 0; i < arr.length; i++)
-    //shuffleCard.push(arr[i], arr[i]);
- // }       
-          
+};
+   
 
 
 
