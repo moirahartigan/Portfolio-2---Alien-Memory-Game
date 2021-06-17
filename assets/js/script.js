2652a86c7bd4df01740f07ad6ad4e426ec5730aa
moirahartigan <moirahartigan@gmail.com>
2021-06-17 14:28:08 +0000

    Remove ternary method from flip card function and revert to previous layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,61 @@
-// Wait for the Dom to finish loading before running the game
-// get button elements and add event listeners to them
-
+// define variables
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false; //used to check if card has already been clicked 
 let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
-let [firstCard, ], secondCard; //Used to check for cards match
+let firstCard, secondCard; //Used to check for cards match
+
 /*
 onclick function for cards, add flip class for css effects
 code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+this.classList.add('flip'); //if valid, flips card using css class
 
-    this.classList.add('flip'); //if valid, flips card using css class
+if (!hasFlippedCard){
+    // first click
+    hasFlippedCard = true;
+    firstCard = this; //stores this as first card
+   } else {
+       //second card
+       hasFlippedCard = false;
+       secondCard = this; 
+       
+       checkforMatch();
+   }
+}
 
-    if (!hasFlippedCard) {
-        // first click
-        hasFlippedCard = true;
-        firstCard = this; //stores this as first card
-        return;
+function checkforMatch(){
+  //  do they match? - if not thay will be flipped back
+  if (firstCard.dataset.image ===
+    secondCard.dataset.image) {
+    disableCards(); 
+    } else {
+    unflipCards();  
     } 
-        //second card
-        secondCard = this;
-    lockBoard = true;
-
-    checkForMatch();
-  }
-
-/*
-ternary operator checks if firstCard & secondCard 'data-image' are a match
-code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
-*/
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-  isMatch ? disableCards() : unflipCards();
 }
-        
-     
-      function disableCards() {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-    
-        resetBoard();
-      }
-          
-        function unflipCards() {
-          setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-      
-            resetBoard();
-          }, 1500);
-        }
- /*
-resetBoardStatus clears values used in flipcard & checkformatch functions
-code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
-*/         
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+
+function disableCards(){
+// if its a match the eventlistener is disabled
+firstCard.removeEventListener('click', flipCard);  
+secondCard.removeEventListener('click', flipCard); 
 }
+
+function unflipCards(){
+ // then if its not a match the flipclass is removed
+ setTimeout(() => {
+    firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');  
+  }, 900);      
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));        
+
+
+
+
+
           
-          (function shuffle() {
-            cards.forEach(card => {
-              let ramdomPos = Math.floor(Math.random() * 16);
-              card.style.order = ramdomPos;
-            });
-          })();
-         
-
-
-          cards.forEach(card => card.addEventListener('click', flipCard));
  // let shuffleCard = [monster-1, monster-1, monster-2, monster-2, monster-3, monster-3, monster-4, monster-4, 
    // monster-5, monster-5, monster-6, monster-6, monster-7, monster-7, monster-8, monster-8];
 
@@ -83,37 +66,6 @@ function resetBoard() {
           
 
 
-// game info -timer 
 
-
-let moveCounter = 0;
-let timer = {
-  seconds: 0,
-  minutes: 0,
-  clearTime: -1
-};
-
-
-//Start time first card is clicked
-
-//Start timer
-let startTimer = function() {
-    if (timer.seconds === 59) {
-      timer.minutes++;
-      timer.seconds = 0;
-    } else {
-      timer.seconds++;
-    }
-
-//  reset Clock back to 0 and restart
-
-function resetTimer() {
-    clearInterval(timer.clearTime);
-    timer.seconds = 0;
-    timer.minutes = 0;
-    $(".timer").text("0:00");
-  
-    timer.clearTime = setInterval(startTimer, 1000);
-  }
 
  
