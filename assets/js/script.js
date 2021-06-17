b2e41dbb4dc7a6aed9941a4b9b56314da7e56cf3
moirahartigan <moirahartigan@gmail.com>
2021-06-17 11:51:09 +0000

    debug JavaScript - code has broken

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,14 @@ const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false; //used to check if card has already been clicked 
 let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
-let firstCard, secondCard; //Used to check for cards match
+let [firstCard, ], secondCard; //Used to check for cards match
 /*
 onclick function for cards, add flip class for css effects
 code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
     this.classList.add('flip'); //if valid, flips card using css class
 
@@ -24,54 +24,55 @@ function flipCard() {
     } 
         //second card
         secondCard = this;
-        
-        checkForMatch();
-    }
+    lockBoard = true;
+
+    checkForMatch();
+  }
 
 /*
 ternary operator checks if firstCard & secondCard 'data-image' are a match
 code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */
 function checkForMatch() {
-    let isMatch = firstCard.dataset.image === secondCard.dataset.image; //checks if dataset.image are a match
-      isMatch ? disableCards() : unflipCards();
-      }
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
         
      
-function disableCards() {
+      function disableCards() {
         firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard);
+    
+        resetBoard();
+      }
           
-        resetGameBoard();
+        function unflipCards() {
+          setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+      
+            resetBoard();
+          }, 1500);
         }
-          
-function unflipCards() {
-        lockBoard = true;
-          
-        setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-          
-        resetGameBoard();
-        lockBoard = false;
-        }, 700);
-          }
  /*
 resetBoardStatus clears values used in flipcard & checkformatch functions
 code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */         
-function resetGameBoard() {
-            [hasFlippedCard, lockBoard] = [false, false];
-            [firstCard, secondCard] = [null, null];
-          }
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
           
-(function shuffle() {
+          (function shuffle() {
             cards.forEach(card => {
-               let randomPos = Math.floor(Math.random() * 16);
-              card.style.order = randomPos;
-             });
+              let ramdomPos = Math.floor(Math.random() * 16);
+              card.style.order = ramdomPos;
+            });
           })();
-  
+         
+
+
+          cards.forEach(card => card.addEventListener('click', flipCard));
  // let shuffleCard = [monster-1, monster-1, monster-2, monster-2, monster-3, monster-3, monster-4, monster-4, 
    // monster-5, monster-5, monster-6, monster-6, monster-7, monster-7, monster-8, monster-8];
 
@@ -80,7 +81,7 @@ function resetGameBoard() {
     //shuffleCard.push(arr[i], arr[i]);
  // }       
           
-cards.forEach(card => card.addEventListener('click', flipCard));
+
 
 // game info -timer 
 
@@ -115,3 +116,4 @@ function resetTimer() {
     timer.clearTime = setInterval(startTimer, 1000);
   }
 
+ 
